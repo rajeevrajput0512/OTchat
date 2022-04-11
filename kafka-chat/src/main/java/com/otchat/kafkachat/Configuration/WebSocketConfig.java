ffbde5abb3b1fd20a67520267429a353ecd84033
Rajeev Rajput <44337221+rajeevrajput0512@users.noreply.github.com>
2022-04-11 13:07:30 +0530

    changed the end-point

--- kafka-chat/src/main/java/com/otchat/kafkachat/Configuration/WebSocketConfig.java
+++ kafka-chat/src/main/java/com/otchat/kafkachat/Configuration/WebSocketConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-           registry.addEndpoint("/endpoint").setAllowedOriginPatterns("*").withSockJS();
+          registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
